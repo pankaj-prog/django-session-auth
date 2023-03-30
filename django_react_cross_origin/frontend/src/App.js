1c2bd0f91a8b41806e29e394dcdd2b9706c0e975
pankaj-prog <wadhwanip38@gmail.com>
2023-03-30 18:45:42 +0530

    update app.js from class component to functional component

--- django_react_cross_origin/frontend/src/App.js
+++ django_react_cross_origin/frontend/src/App.js
@@ -1,162 +1,172 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-class App extends React.Component {
+function App() {
+  const [csrf, setCsrf] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      csrf: "",
-      username: "",
-      password: "",
-      error: "",
-      isAuthenticated: false,
-    };
-  }
-
-  componentDidMount = () => {
-    this.getSession();
-  }
-
-  getCSRF = () => {
+  const getCSRF = () => {
     fetch("http://localhost:8000/api/csrf/", {
       credentials: "include",
     })
-    .then((res) => {
-      let csrfToken = res.headers.get("X-CSRFToken");
-      this.setState({csrf: csrfToken});
-      console.log(csrfToken);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  }
+      .then((res) => {
+        let csrfToken = res.headers.get("X-CSRFToken");
+        setCsrf(csrfToken);
+        console.log(csrfToken);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  getSession = () => {
+  const getSession = () => {
     fetch("http://localhost:8000/api/session/", {
       credentials: "include",
     })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      if (data.isAuthenticated) {
-        this.setState({isAuthenticated: true});
-      } else {
-        this.setState({isAuthenticated: false});
-        this.getCSRF();
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  }
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.isAuthenticated) {
+          setIsAuthenticated(true);
+        } else {
+          setIsAuthenticated(false);
+          getCSRF();
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  whoami = () => {
+  const whoami = () => {
     fetch("http://localhost:8000/api/whoami/", {
       headers: {
         "Content-Type": "application/json",
       },
       credentials: "include",
     })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log("You are logged in as: " + data.username);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  }
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("You are logged in as: " + data.username);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  handlePasswordChange = (event) => {
-    this.setState({password: event.target.value});
-  }
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
-  handleUserNameChange = (event) => {
-    this.setState({username: event.target.value});
-  }
+  const handleUserNameChange = (event) => {
+    setUsername(event.target.value);
+  };
 
-  isResponseOk(response) {
+  const isResponseOk = (response) => {
     if (response.status >= 200 && response.status <= 299) {
       return response.json();
     } else {
       throw Error(response.statusText);
     }
-  }
+  };
 
-  login = (event) => {
+  const login = (event) => {
     event.preventDefault();
     fetch("http://localhost:8000/api/login/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "X-CSRFToken": this.state.csrf,
+        "X-CSRFToken": csrf,
       },
       credentials: "include",
-      body: JSON.stringify({username: this.state.username, password: this.state.password}),
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
     })
-    .then(this.isResponseOk)
-    .then((data) => {
-      console.log(data);
-      this.setState({isAuthenticated: true, username: "", password: "", error: ""});
-    })
-    .catch((err) => {
-      console.log(err);
-      this.setState({error: "Wrong username or password."});
-    });
-  }
+      .then(isResponseOk)
+      .then((data) => {
+        console.log(data);
+        setIsAuthenticated(true);
+        setUsername("");
+        setPassword("");
+        setError("");
+      })
+      .catch((err) => {
+        setError("Wrong username or password.");
+      });
+  };
 
-  logout = () => {
+  const logout = () => {
     fetch("http://localhost:8000/api/logout", {
       credentials: "include",
     })
-    .then(this.isResponseOk)
-    .then((data) => {
-      console.log(data);
-      this.setState({isAuthenticated: false});
-      this.getCSRF();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+      .then(isResponseOk)
+      .then((data) => {
+        console.log(data);
+        setIsAuthenticated(false);
+        getCSRF();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
-  render() {
-    if (!this.state.isAuthenticated) {
-      return (
-        <div className="container mt-3">
-          <h1>React Cookie Auth</h1>
-          <br />
-          <h2>Login</h2>
-          <form onSubmit={this.login}>
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input type="text" className="form-control" id="username" name="username" value={this.state.username} onChange={this.handleUserNameChange} />
-            </div>
-            <div className="form-group">
-              <label htmlFor="username">Password</label>
-              <input type="password" className="form-control" id="password" name="password" value={this.state.password} onChange={this.handlePasswordChange} />
-              <div>
-                {this.state.error &&
-                  <small className="text-danger">
-                    {this.state.error}
-                  </small>
-                }
-              </div>
-            </div>
-            <button type="submit" className="btn btn-primary">Login</button>
-          </form>
-        </div>
-      );
-    }
+  useEffect(() => {
+    getSession();
+  }, []);
+
+  if (isAuthenticated) {
     return (
       <div className="container mt-3">
         <h1>React Cookie Auth</h1>
-        <p>You are logged in!</p>
-        <button className="btn btn-primary mr-2" onClick={this.whoami}>WhoAmI</button>
-        <button className="btn btn-danger" onClick={this.logout}>Log out</button>
+        <br />
+        <h2>Login</h2>
+        <form onSubmit={login}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              className="form-control"
+              id="username"
+              name="username"
+              value={username}
+              onChange={handleUserNameChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="username">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            <div>{error && <small className="text-danger">{error}</small>}</div>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
+        </form>
       </div>
-    )
+    );
   }
+  return (
+    <div className="container mt-3">
+      <h1>React Cookie Auth</h1>
+      <p>You are logged in!</p>
+      <button className="btn btn-primary mr-2" onClick={whoami}>
+        WhoAmI
+      </button>
+      <button className="btn btn-danger" onClick={logout}>
+        Log out
+      </button>
+    </div>
+  );
 }
 
 export default App;
